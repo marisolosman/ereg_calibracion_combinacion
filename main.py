64a2e77e96d7f6e569dcd0633b95f30e31ea6133
marisolosman <marisolosman17@gmail.com>
2018-04-06 00:01:39 -0300

    mejorando el main y emprolijando el model

--- main.py
+++ main.py
@@ -8,116 +8,12 @@ import numpy as np
 import model #objeto y metodos asociados a los modelos
 import observation # idem observaciones
 import glob #listar archivos
-import os.path #manejar path
 import calendar #manejar meses del calendario
 from pathlib import Path #manejar path
 
-def calibrar(modelos,instit,latn, lonn, miembros, plazos, fechai, fechaf, extension, varn, ic_mes,leadtime,sss, p, lat_sur, lat_nor, lon_oes, lon_est):
-
-    for i in np.arange(len(modelos)):
-
-        #instancio observacion
-
-        archivo = Path('/datos/osman/nmme_output/obs_'+varn+'_'+sss+'.npz')
-
-        output = Path('/datos/osman/nmme_output/cal_forecasts/'+varn+'_'+ modelos[i] +'_'+
-                '{:02}'.format(ic_mes)+'_'+'{:02}'.format(leadtime)+'_01_'+'{:03}'.format(p)+'_hind.npz')
-
-        if archivo.is_file():
-
-            data = np.load(archivo)
-
-            obs_dt = data['obs_dt']
-
-            terciles = data ['terciles']
-
-            data.close()
-
-        else:
-
-            obs = observation.Observ('prec','cpc','prate','Y','X',1982,2011)
-
-            #selecciono trimestre de interes
-
-            print("Selecciono observacion")
-
-            [lats_obs, lons_obs, obs_3m] = obs.select_months(ic_mes,leadtime, lat_sur, lat_nor, lon_oes, lon_est)
-
-            #remuevo tendencia y estandarizo observacion
-
-            print("Remuevo tendencia obs")
-
-            obs_dt = obs.remove_trend(obs_3m, True) #la variable logica habilita o no CV
-
-            print(obs_dt.shape)
-
-            #obtengo los limites de los terciles
-
-            print("Calculo terciles")
-
-            terciles = obs.computo_terciles(obs_dt,True) # la variable logica habilita o no CV
-
-            print("Calculo que tercil se observo en cada anio")
-
-            categoria_obs = obs.computo_categoria (obs_dt, terciles)
-
-            np.savez(archivo, obs_dt = obs_dt,
-                    lats_obs = lats_obs, lons_obs = lons_obs, terciles = terciles, cat_obs =
-                    categoria_obs)
-
-        #instancio modelo 
-
-        if output.is_file():
-            pass
-        else:
-
-            modelo = model.Model (modelos[i],instit[i],varn,latn[i],lonn[i],miembros[i],plazos[i],fechai[i],fechaf[i], extension[i])
-
-            print (modelo)
-
-            #tomo los pronos segun la ci y el leadtime
-
-            print("Selecciono pronos")
-
-            [lats,lons,pronos] = modelo.select_months(ic_mes, leadtime, lat_sur, lat_nor, lon_oes, lon_est)
-        
-            print("Remuevo tendencia")
-
-            pronos_dt = modelo.remove_trend(pronos, True)
-
-            #aplico ereg:  el imput es el modelo y las observaciones estandarizadas y la proporcion
-            #de kmax a aplicar en caso de correccion de ensamble
-            #mientras que el output es el valor y' Kmax admitido espb Rmean
-
-            print("aplico ensemble regression")
-
-            [forecast_cr, Rmedio, Rmej, epsb, Kmax, K] = modelo.ereg(pronos_dt, obs_dt,p, True)
-
-            #incluye parametro p entre 0 y 1 que representa la proporcion del factor de inflacion kmax
-            #que le aplico al ensamble, siguiendo a unger et al 2008
-            
-            #calculo la probabilidad de pp para cada tercil, cada anio y cada miembro del modelo
-
-            prob_terc = modelo.probabilidad_terciles (forecast_cr, epsb, terciles)
-            
-            #ahora deberia pesar cada modelo de acuerdo a la historia previa. 
-            #genero un metodo para evaluar la pdf en el valor de la observacion
-            #OJO SOLO ESTOY PROGRAMANDO UN TIPO DE PESO PORQUE EL OTRO ESTA BASADO EN Rmedio QUE YA 
-            #LO OBTUVE
-
-            #creo que chace the rapper puede gustarme, pero te odio lana del rey
-            print("estimo peso del modelo")
-
-            pdf_intensity = modelo.pdf_eval(forecast_cr,epsb,obs_dt)
-
-            #guardo el pronostico calibrado y la intensidad de la pdf
-
-            np.savez(output,lats = lats, lons = lons, pronos_dt = pronos_dt, pronos_cr = forecast_cr, eps = epsb,Rm = Rmedio, Rb = Rmej, K2 = Kmax, 
-                    K = K, peso = pdf_intensity, prob_terc = prob_terc)
-
 def main():
   
-	# Define parser data
+    #Define parser data
     parser = argparse.ArgumentParser(description='Calibrating models using Ensemble Regression.')
     # First arguments. Variable to calibrate. Prec or temp
     parser.add_argument('variable',type=str, nargs= 1,\
@@ -134,86 +30,105 @@ def main():
 
     # Extract dates from args
     args=parser.parse_args()
-
-    varn = args.variable[0]
-    
-    ic_mes = args.IC[0]
-    
-    plazo = args.leadtime[0]
-    
     p = args.spread[0]
 
-    seas = range(ic_mes+plazo,ic_mes+plazo+3)
+    seas = range(args.IC[0]+args.leadtime[0],args.IC[0]+args.leadtime[0]+3)
        
     sss = [i-12 if i>12 else i for i in seas ]
     sss = "".join(calendar.month_abbr[i][0] for i in sss)
 
     lista = glob.glob("/home/osman/actividades/postdoc/modelos/*")
-    
-    modelo = []
-    instit = []
-    latn = []
-    lonn = []
-    miembros = []
-    plazos = []
-    fechai = []
-    fechaf = []
-    ext = []
-    
+   
     if args.no_model is not None: #si tengo que descartar modelos
+        
+        lista = [i for i in lista if [line.rstrip('\n') for line in open(i)][0] not in args.no_model]
+        print(lista)
 
-        nombres = args.no_model[:]
-
-        for j in nombres:
-            for i in lista:
-                lines = [line.rstrip('\n') for line in open(i)]
-                modelos = lines[0] ==j
-
-                if modelos: 
-                    
-                    lista.remove(i)
-                    break
+    keys = ['nombre', 'instit', 'latn', 'lonn', 'miembros', 'plazos', 'fechai', 'fechaf','ext']
+    modelos = []
 
     for i in lista:
        
         lines = [line.rstrip('\n') for line in open(i)]
-        modelo.append(lines[0])
-        instit.append(lines[1])
-        latn.append(lines[2])
-        lonn.append(lines[3])
-        miembros.append(int(lines[4]))
-        plazos.append(int(lines[5]))
-        fechai.append(int(lines[6]))
-        fechaf.append(int(lines[7]))
-        ext.append(lines[8])
+        modelos.append(dict(zip(keys,[lines[0],lines[1],lines[2],lines[3],int(lines[4]),int(lines[5]),int(lines[6]),int(lines[7]),lines[8]])))
         
-#llamo a la funcion para calibrar
-#def calibrar(modelos,instit,latn, lonn, miembros, plazos, fechai, fechaf, extension, varn, ic_mes,leadtime,sss, p, lat_sur, lat_nor, lon_oes, lon_est):
+ 
+    archivo = Path('/datos/osman/nmme_output/obs_'+args.variable[0]+'_'+sss+'.npz')
 
-    calibrar(modelo,instit,latn, lonn, miembros, plazos, fechai,fechaf, ext, varn,ic_mes,plazo, sss, p,lat_sur, lat_nor, lon_oes, lon_est) 
+    if archivo.is_file():
+
+        data = np.load(archivo)
+        obs_dt = data['obs_dt']
+        terciles = data ['terciles']
+        data.close()
+
+    else:
+
+        obs = observation.Observ(args.variable[0],'cpc','prate','Y','X',1982,2011)
+        #selecciono trimestre de interes
+        print("Selecciono observacion")
+        [lats_obs, lons_obs, obs_3m] = obs.select_months(args.IC[0],args.leadtime[0], coords['lat_s'], coords['lat_n'], coords['lon_w'], coords['lon_e'])
+        #remuevo tendencia y estandarizo observacion
+        print("Remuevo tendencia obs")
+        obs_dt = obs.remove_trend(obs_3m, True) #la variable logica habilita o no CV
+        print(obs_dt.shape)
+        #obtengo los limites de los terciles
+        print("Calculo terciles")
+        terciles = obs.computo_terciles(obs_dt,True) # la variable logica habilita o no CV
+        print("Calculo que tercil se observo en cada anio")
+        categoria_obs = obs.computo_categoria (obs_dt, terciles)
+        np.savez(archivo, obs_dt = obs_dt,
+                lats_obs = lats_obs, lons_obs = lons_obs, terciles = terciles, cat_obs =
+                categoria_obs)
+    for it in modelos:
+
+        output = Path('/datos/osman/nmme_output/cal_forecasts/'+args.variable[0]+'_'+ it['nombre'] +'_'+
+                calendar.month_abbr[args.IC[0]] +'_'+ sss +'_01_'+'{:03}'.format(p)+'_hind.npz')
+
+        #instancio modelo 
+        if output.is_file():
+            pass
+        else:
+
+            modelo = model.Model (it['nombre'],it['instit'],args.variable[0],it['latn'],it['lonn'],it['miembros'],it['plazos'],it['fechai'],it['fechaf'], it['ext'])
+            print (modelo)
+            #tomo los pronos segun la ci y el leadtime
+            print("Selecciono pronos")
+            [lats,lons,pronos] = modelo.select_months(args.IC[0], args.leadtime[0], coords['lat_s'], coords['lat_n'], coords['lon_w'], coords['lon_e'])
+            print("Remuevo tendencia")
+            pronos_dt = modelo.remove_trend(pronos, True)
+            #aplico ereg:  el imput es el modelo y las observaciones estandarizadas y la proporcion
+            #de kmax a aplicar en caso de correccion de ensamble
+            #mientras que el output es el valor y' Kmax admitido espb Rmean
+            print("aplico ensemble regression")
+            [forecast_cr, Rmedio, Rmej, epsb, Kmax, K] = modelo.ereg(pronos_dt, obs_dt,p, True)
+            #incluye parametro p entre 0 y 1 que representa la proporcion del factor de inflacion kmax
+            #que le aplico al ensamble, siguiendo a unger et al 2008
+          
+            #calculo la probabilidad de pp para cada tercil, cada anio y cada miembro del modelo
+            prob_terc = modelo.probabilidad_terciles (forecast_cr, epsb, terciles)
+            #ahora deberia pesar cada modelo de acuerdo a la historia previa. 
+            #genero un metodo para evaluar la pdf en el valor de la observacion
+            #OJO SOLO ESTOY PROGRAMANDO UN TIPO DE PESO PORQUE EL OTRO ESTA BASADO EN Rmedio QUE YA 
+            #LO OBTUVE
+            print("estimo peso del modelo")
+            pdf_intensity = modelo.pdf_eval(forecast_cr,epsb,obs_dt)
+            #guardo el pronostico calibrado y la intensidad de la pdf
+            np.savez(output,lats = lats, lons = lons, pronos_dt = pronos_dt, pronos_cr = forecast_cr, eps = epsb,Rm = Rmedio, Rb = Rmej, K2 = Kmax, 
+                    K = K, peso = pdf_intensity, prob_terc = prob_terc)
+
 start = time.time()
 
-#abro archivo donde guardo coordenadas
-coordenadas = 'coords'
-
-lines = [line.rstrip('\n') for line in open(coordenadas)]
-
-lat_sur = float(lines[1]) #se va a un text file
-lat_nor = float(lines [2]) #idem
-lon_oes = float(lines[3]) #idem
-lon_est = float(lines[4]) #idem
-
-#modelo = ['CFSv2','CESM1', 'CanCM3', 'CanCM4', 'CM2p1', 'FLOR-A06', 'FLOR-B01', 'GEOS5']
-#instit = ['NCEP', 'NCAR', 'CMC','CMC', 'GFDL','GFDL', 'GFDL', 'NASA']
-#latn = [ 'Y', 'Y', 'lat','lat', 'Y', 'Y', 'lat','Y' ]
-#lonn = [ 'X', 'X', 'lon', 'lon', 'X', 'X', 'lon', 'X']
-#miembros = [ 24, 28, 10, 10, 28, 28, 10, 28 ]
-#plazos = [ 10, 12, 12, 12, 12, 12, 12, 9 ]
-#fechai = [ 1982, 1982, 1982, 1982, 1982, 1982, 1982, 1982]
-#fechaf = [ 2010, 2010, 2010, 2010, 2010, 2010, 2010, 2010]
-#ext = ['nc','nc', 'nc4', 'nc4', 'nc', 'nc','nc', 'nc']
-#main(modelo,instit,latn, lonn, miembros, plazos, fechai,fechaf, ext, varn, sss, p) 
 if __name__=="__main__":
+    #abro archivo donde guardo coordenadas
+    coordenadas = 'coords'
+
+    lines = [line.rstrip('\n') for line in open(coordenadas)]
+
+    coords = {'lat_s': float(lines[1]), 
+            'lat_n': float(lines [2]),
+            'lon_w': float(lines[3]),
+            'lon_e': float(lines[4])}
     main()
 end = time.time()
 
--- model.py
+++ model.py
@@ -12,15 +12,11 @@ cores = 9
 def manipular_nc(archivo,variable,lat_name,lon_name,lats, latn, lonw, lone):
 
     dataset = xr.open_dataset(archivo,decode_times = False)
-
     var_out = dataset[variable].sel(**{lat_name: slice(lats,latn), lon_name: slice(lonw,lone)})
-
     lon = dataset[lon_name].sel(**{lon_name: slice(lonw,lone)})
-    
     lat = dataset[lat_name].sel(**{lat_name: slice(lats,latn)})
 
     return var_out, lat, lon
-
 
 class Model(object):
 #caracteristicas comunes de todos los modelos
@@ -46,7 +42,6 @@ class Model(object):
                 self.institution, self.ensembles, self.leadtimes)
 
 #comienzo la definición de los métodos
-
 #selecciono los datos a partir del mes de IC y plazo, saco promedio trimestral
     
     def select_months(self, init_cond, target, lats, latn, lonw, lone):  
@@ -61,14 +56,10 @@ class Model(object):
             flag_end = 0
 	
         ruta = '/datos/osman/nmme/monthly/'       
-
-        #abro un archivo de ejempl
+        #abro un archivo de ejemplo
         file = ruta + 'prec_Amon_' + self.institution + '-' + self.name + '_'+'198201_r1i1p1_'+'198201-198212.'+ self.ext
-
         hindcast_length = self.hind_end - self.hind_begin + 1 
-
         [variable,latitudes,longitudes] = manipular_nc (file,self.var_name,self.lat_name,self.lon_name,lats,latn,lonw,lone)
-
         forecast = np.empty([hindcast_length,self.ensembles,latitudes.shape[0],longitudes.shape[0]])
 
         #loop sobre los anios del hindcast period
@@ -76,7 +67,6 @@ class Model(object):
             
             for j in np.arange(1,self.ensembles+1):
 
-                               
                 file = ruta + 'prec_Amon_' + self.institution + '-' + self.name + '_' + str(
                         i) + '{:02d}'.format(init_cond)+'_r'+str(j)+'i1p1_'+str(i
                                 )+'{:02d}'.format(init_cond) + '-' + str(i+
@@ -97,8 +87,6 @@ class Model(object):
                         forecast[ i-self.hind_begin, j-1,:,:] = np.NaN
                         print(i,j)                        
     
-               #mes, target no hay que correrlo
-
                 variable = []
 	# Return values of interest: latitudes longitudes forecast
         return latitudes, longitudes, forecast
@@ -109,48 +97,33 @@ class Model(object):
         #CV_opt boolean
 
         [ntimes,nmembers,nlats,nlons] = forecast.shape
-
         anios = np.arange(ntimes)
 
         i = np.repeat(np.arange(ntimes,dtype = int),nmembers*nlats*nlons)
-
         l = np.tile(np.repeat(np.arange(nmembers,dtype = int),nlats*nlons),ntimes)
-
         j = np.tile(np.repeat(np.arange(nlats,dtype = int),nlons),ntimes*nmembers)
-
         k = np.tile(np.arange(nlons,dtype = int),ntimes*nmembers*nlats)
 
         p = Pool(cores)
-
         p.clear()
 
         if CV_opt: #validacion cruzada ventana 1 anio
 
             print("Validacion cruzada")
-
             CV_matrix = np.logical_not(np.identity(ntimes))
 
             def filtro_tendencia(i,l,j,k,anios=anios,CV_m = CV_matrix,forec=forecast): #forecast 4D
 
-                y = np.nanmean(forec[:,:,j,k], axis = 1)
-
-                A = np.array([ anios[CV_m[:,i]], np.ones((anios.shape[0]-1))])
-
-                d = np.linalg.lstsq(A.T,y[CV_m[:,i]])[0]
-
-                for_dt = forec[i,l,j,k]-(d[0]*anios[i]+d[1])
+                y = np.nanmean(forec[:,:,j,k], axis = 1) #media del ensamble
+                A = np.vstack([ anios[CV_m[:,i]], np.ones((anios.shape[0]-1))])
+                m, c = np.linalg.lstsq(A.T,y[CV_m[:,i]])[0]
+                for_dt = forec[i,l,j,k]-(m*anios[i]+c)
 
                 return for_dt
 
             res = p.map(filtro_tendencia,i.tolist(),l.tolist(),j.tolist(),k.tolist())
-            
             forecast_dt = np.reshape(np.squeeze(np.stack(res)),[ntimes,nmembers,nlats,nlons])
-            #forecast_dt = np.squeeze(np.stack(res))
-
-            print(forecast_dt.shape)
-
             del(filtro_tendencia,res)
-
             p.close()
 
         else:
@@ -158,27 +131,17 @@ class Model(object):
             def filtro_tendencia(i, l, j, k, anios = anios, forec = forecast): #forecast 4D
 
                 y = np.nanmean(forec[:,:,j,k], axis = 1)
-
-                A = np.array([ anios, np.ones(anios.shape[0])])
-
-                d = np.linalg.lstsq(A.T,y)[0]
-
-                for_dt = forec[i,l,j,k]-(d[0]*anios[i]+d[1])
+                A = np.vstack([ anios, np.ones(anios.shape[0])])
+                m, c = np.linalg.lstsq(A.T,y)[0]
+                for_dt = forec[i,l,j,k]-(m*anios[i]+c)
 
                 return for_dt
 
             print("Paralelizando")
-
             res = p.map(filtro_tendencia, i.tolist(), l.tolist(), j.tolist(), k.tolist())
-            
             print("Termino Paralelizacion")
-
             forecast_dt = np.reshape(np.squeeze(np.stack(res)),[ntimes, nmembers, nlats, nlons])
-            
-            print(forecast_dt.shape)
-
             del(filtro_tendencia,res)
-
             p.close()
             
         return forecast_dt
@@ -190,102 +153,64 @@ class Model(object):
         #quiero o no hacerlo en cross validacion
 
         [ntimes,nmembers,nlats,nlons] = forecast.shape
-
         #calculo primero si necesito corregir mi ensamble
-
         em = np.nanmean(forecast, axis = 1) #media del ensamble
-
         Rm = np.nanmean((observation - np.nanmean(observation, axis = 0 )) * 
                 (em-np.nanmean(em, axis = 0)), axis = 0) / (np.nanstd(em, axis = 0) * 
                         np.nanstd(observation, axis = 0)) #correlacion entre ens mean y obs
 
         signal = np.nanvar (em, axis = 0) #senial: varianza de la media del ensamble
-
         noise = np.nanmean(np.nanvar(forecast, axis = 1), axis = 0) #media de la dispersion entre miembros
-
         #Rbest = Rm * sqrt( 1 +(m/(m-1) * N) /S )
-        
         Rbest = Rm * np.sqrt(1+(self.ensembles/(self.ensembles-1) * noise)/signal)
-
         #epsbest = n/(n-1) * Varobs * (1-Rmean**2)
-
-        epsbn =((ntimes-1)/(ntimes-2)) *  np.nanvar(observation, axis = 0)*(1-np.power(Rbest,2))
-
+        epsbn =((ntimes)/(ntimes-1)) *  np.nanvar(observation, axis = 0)*(1-np.power(Rbest,2))
         #kmax**2 S/N * (m-1/m) * (1/R**2-1)
         kmax = signal/noise * (((self.ensembles-1)/self.ensembles) * (1/np.power(Rm,2)-1))
-
         # si kmax es amayor a 1 lo fuerzo a que sea 1
-
         kmax [np.where(kmax>1)] = 1
-
         #testeo
-
         K = np.zeros_like(epsbn)
-
-        #si el error en la regresion positivo, entonces no cambio la dispersion del ensamble
-
+        #si el error en la regresion positivo, entonces cambio la dispersion del ensamble segun el parametro p
         K[np.where(epsbn>=0)] = 1 * P
-
         #si el error en la regresion es negativo, voy a aplicar ereg solo con la media
-
         K[np.where(epsbn<0)] = 0
-        
-
         K = np.repeat(np.repeat(K[np.newaxis,:,:], self.ensembles, axis=0)[np.newaxis,:,:,:],ntimes,axis=0)
-
         forecast_inf = np.repeat(np.nanmean(forecast, axis =1)[:,np.newaxis,:,:],
                 nmembers,axis=1)*(1-K)+forecast*K
-
         #calulo otra vez los parametros que cambiaron
-
         noise = np.nanmean(np.nanvar(forecast_inf, axis = 1), axis = 0) #media de la dispersion entre miembros
-
         #Rbest = Rm * sqrt( 1 +(m/(m-1) * N) /S )
-        
         Rbest = Rm * np.sqrt(1+(self.ensembles/(self.ensembles-1) * noise)/signal)
-
         #epsbest = n/(n-1) * Varobs * (1-Rmean**2)
-
-        epsbn =((ntimes-1)/(ntimes-2)) *  np.nanvar(observation, axis = 0)*(1-np.power(Rbest,2))
+        epsbn =((ntimes)/(ntimes)) *  np.nanvar(observation, axis = 0)*(1-np.power(Rbest,2))
 
         #ahora calculo la regresion
 
         i = np.repeat(np.arange(ntimes,dtype = int),nmembers*nlats*nlons)
-
         l = np.tile(np.repeat(np.arange(nmembers,dtype = int),nlats*nlons),ntimes)
-
         j = np.tile(np.repeat(np.arange(nlats,dtype = int),nlons),ntimes*nmembers)
-
         k = np.tile(np.arange(nlons,dtype = int),ntimes*nmembers*nlats)
-
         p = Pool(cores)
-
         p.clear()
 
         if CV_opt: #validacion cruzada ventana 1 anio
 
             print("Validacion cruzada")
-
             CV_matrix = np.logical_not(np.identity(ntimes))
 
             def ens_reg(i,l,j,k,CV_m = CV_matrix,obs = observation, forec=forecast_inf): #forecast 4D
 
                 y = np.nanmean(forec[:,:,j,k], axis = 1)
-
-                A = np.array([ y[CV_m[:,i]], np.ones((y.shape[0]-1))])
-
-                d = np.linalg.lstsq(A.T,obs[CV_m[:,i],j,k])[0]
-
-                for_cr = (d[0]*forec[i,l,j,k]+d[1])
+                A = np.vstack([ y[CV_m[:,i]], np.ones((y.shape[0]-1))])
+                m, c = np.linalg.lstsq(A.T, obs[CV_m[:,i],j,k])[0]
+                for_cr = m * forec[i,l,j,k] + c 
 
                 return for_cr
 
             res = p.map(ens_reg, i.tolist(), l.tolist(), j.tolist(), k.tolist())
-            
             forecast_cr = np.reshape(np.squeeze(np.stack(res)),[ntimes,nmembers,nlats,nlons])
-
             del(ens_reg,res)
-
             p.close()
 
         else:
@@ -293,32 +218,21 @@ class Model(object):
             def ens_reg(i, l, j, k, obs = observation, forec = forecast_inf): #forecast 4D
 
                 y = np.nanmean(forec[:,:,j,k], axis = 1)
-
-                A = np.array([ y, np.ones(y.shape[0])])
-
-                d = np.linalg.lstsq(A.T,obs[:,j,k])[0]
-
-                for_cr = (d[0]*forec[i,l,j,k]+d[1])
+                A = np.vstack([ y, np.ones(y.shape[0])])
+                m, c = np.linalg.lstsq(A.T, obs[:,j,k])[0]
+                for_cr = m * forec[i,l,j,k] + c
 
                 return for_cr
 
             print("Paralelizando")
-
             res = p.map(ens_reg, i.tolist(), l.tolist(), j.tolist(), k.tolist())
-            
             print("Termino Paralelizacion")
-
             forecast_cr = np.reshape(np.squeeze(np.stack(res)),[ntimes,nmembers,nlats,nlons])
-            
             print(forecast_cr.shape)
-
             del(ens_reg,res)
-
             p.close()
                   
-        
         return forecast_cr, Rm, Rbest, epsbn, kmax, K
-
 
     def pdf_eval(self,forecast,eps,observation):
 
@@ -327,29 +241,19 @@ class Model(object):
         [ntimes,nmembers,nlat,nlon] = forecast.shape
 
         i = np.repeat(np.arange(ntimes,dtype = int),nmembers*nlat*nlon)
-
         l = np.tile(np.repeat(np.arange(nmembers,dtype = int),nlat*nlon),ntimes)
-
         j = np.tile(np.repeat(np.arange(nlat,dtype = int),nlon),ntimes*nmembers)
-
         k = np.tile(np.arange(nlon,dtype = int),ntimes*nmembers*nlat)
-          
         p = Pool(cores)
-
         p.clear()
 
         def evaluo_pdf_normal(i,l,j,k,obs = observation,media = forecast,sigma = eps):
 
             with warnings.catch_warnings():
-
                 warnings.filterwarnings('error')
-
                 try:
-
                     pdf_intensity = norm.pdf(obs[i,j,k], loc = media[i,l,j,k],scale = np.sqrt(sigma[j,k]))
-
                 except RuntimeWarning:
-
                     pdf_intensity = np.NaN
                     print(sigma[j,k],j,k)
 
@@ -357,17 +261,13 @@ class Model(object):
 
 
         res = p.map (evaluo_pdf_normal, i.tolist(), l.tolist(), j.tolist(), k.tolist())
-
         p.close()
-        
         pdf_intensity = np.reshape(np.squeeze(np.stack(res)),[ntimes,nmembers,nlat,nlon])
-
         del(p,res,evaluo_pdf_normal)
 
         #return res
 
         return pdf_intensity
-
 
     def probabilidad_terciles (self,forecast,epsilon,tercil):
         #calcula la probabilidad acumulada hasta los limites de cada uno de los terciles
@@ -376,33 +276,20 @@ class Model(object):
         [ntimes, nmembers, nlats, nlons] = forecast.shape
 
         i = np.repeat(np.arange(ntimes,dtype = int),nmembers*nlats*nlons)
-
-        l = np.tile(np.repeat(np.arange(nmembers,dtype = int),
-            nlats*nlons),ntimes)
-
-        j = np.tile(np.repeat(np.arange(nlats,dtype = int),
-            nlons),ntimes*nmembers)
-
-        k = np.tile(np.arange(nlons,dtype = int),
-                ntimes*nmembers*nlats)
-
+        l = np.tile(np.repeat(np.arange(nmembers,dtype = int),nlats*nlons),ntimes)
+        j = np.tile(np.repeat(np.arange(nlats,dtype = int),nlons),ntimes*nmembers)
+        k = np.tile(np.arange(nlons,dtype = int),ntimes*nmembers*nlats)
         p = Pool(cores)
-
         p.clear()
 
         def evaluo_pdf_normal(i,l,j,k, terc = tercil,media = forecast,sigma = epsilon):
 
-            pdf_cdf = norm.cdf(terc[:,i,j,k], loc = media[i,l,j,k],
-
-                    scale = np.sqrt(sigma[j,k]))
+            pdf_cdf = norm.cdf(terc[:,i,j,k], loc = media[i,l,j,k],scale = np.sqrt(sigma[j,k]))
 
             return pdf_cdf
 
-        res = p.map (evaluo_pdf_normal, i.tolist(), l.tolist(),
-                j.tolist(), k.tolist())
-
+        res = p.map (evaluo_pdf_normal, i.tolist(), l.tolist(),j.tolist(), k.tolist())
         p.close()
-
         prob_terciles = np.rollaxis(np.reshape(np.squeeze(np.stack(res)),
             [ntimes,nmembers,nlats,nlons,2]),4,0)
 
