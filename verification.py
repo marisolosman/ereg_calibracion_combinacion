2ce9a58e43298ab1cabee8ee17960062b854ff23
marisolosman <marisolosman17@gmail.com>
2018-06-15 17:55:39 -0300

    1st coomit verification scores for consolidated forecast

---
+++ verification.py
@@ -0,0 +1,175 @@
+"""
+codigo para calcular la probabilidad pronosticada para cada tercil en cada anio
+a partir de la combinacion de pronosticos con la metodologia de wpdf y wsereg
+modelos calibrados con ereg. Ademas grafica algunos indices de verificacion y guarda los resultados
+"""
+#!/usr/bin/env python
+
+import argparse #parse command line options
+import time #test time consummed
+import numpy as np
+import glob 
+from pathlib import Path 
+import calendar
+import ereg #apply ensemble regression to multi-model ensemble
+import verif_scores #calculo y grafico de los indices de verificacion
+
+def main():
+    # Define parser data
+    parser = argparse.ArgumentParser(description='Combining models')
+    parser.add_argument('variable',type=str, nargs= 1,\
+            help='Variable to calibrate (prec or temp)')
+    parser.add_argument('IC', type = int, nargs= 1,\
+            help = 'Month of intial conditions (from 1 for Jan to 12 for Dec)')
+    parser.add_argument('leadtime', type = int, nargs = 1,\
+            help = 'Forecast leatime (in months, from 1 to 7)')
+    parser.add_argument('mod_spread',  type = float, nargs = 1,\
+            help = 'percentage of spread to retain in each model (from 0 to 1)')
+    subparsers = parser.add_subparsers(help = "Combination technique")
+    wpdf_parser = subparsers.add_parser('wpdf', help = 'weighted sum of calibrated PDFs')
+    wsereg_parser = subparsers.add_parser('wsereg', help = 'Ereg with the weighted superensemble')
+    count_parser = subparsers.add_parser('count', help = 'Count members in each bin')
+    count_parser.set_defaults(ctech = 'count', wtech = ['same']) 
+    wpdf_parser.set_defaults(ctech = 'wpdf')
+    wpdf_parser.add_argument("--weight_tech", required = True, nargs = 1,\
+            choices = ['pdf_int', 'mean_cor', 'same'], dest = 'wtech')
+    wsereg_parser.set_defaults(ctech = 'wsereg')
+    wsereg_parser.add_argument('mme_spread', type = float, default = 1, nargs = 1,\
+            help = 'percentage of spread to retain in the mme (from 0 to 1)')
+    wsereg_parser.add_argument("--weight_tech", required = True, nargs = 1,\
+            choices = ['pdf_int', 'mean_cor', 'same'], dest= 'wtech', help = 'Relative weight between models')
+    # Extract dates from args
+    args=parser.parse_args()
+    nyears = int(modelos[0]['fechaf'] - modelos[0]['fechai'] + 1)
+
+    #defino ref dataset y target season
+    seas = range(args.IC[0] + args.leadtime[0], args.IC[0] + args.leadtime[0] + 3)
+    sss = [i-12 if i>12 else i for i in seas]
+    year_verif = 1982 if seas[0]<12 else 1983
+    SSS = "".join(calendar.month_abbr[i][0] for i in sss)
+   
+    #obtengo datos observados
+    archivo = Path('/datos/osman/nmme_output/obs_'+args.variable[0]+'_'+ str(year_verif) + '_' + SSS + '.npz')
+    data = np.load(archivo)
+    obs_terciles = data ['cat_obs']
+
+    #abro el prono consolidado
+    route = '/datos/osman/nmme_output/comb_forecast/'
+
+    if args.ctech == 'wsereg':
+        file_end = args.variable[0] + '_mme_' + calendar.month_abbr[args.IC[0]] + '_' + SSS +\
+                '_gp_01_p_' + '{:03}'.format(args.mod_spread[0]) +'_'+ args.wtech[0]+'_'+\
+                args.ctech + '_p_'+'{:03}'.format(args.mme_spread[0])+'_hind'
+    else:
+        file_end = args.variable[0] + '_mme_' + calendar.month_abbr[args.IC[0]] + '_' + SSS +\
+                '_gp_01_p_' + '{:03}'.format(args.mod_spread[0]) +'_'+ args.wtech[0]+'_'+ args.ctech + '_hind'
+
+    data = np.load(route + archivo + '.npz'), prob_terc_comb = prob_terc_comb, lat = lat, lon= lon)
+    lat = data ['lat']
+    lon = data ['lon']
+    prob_terc_comb = data ['prob_terc_comb']
+    data = []
+ 
+# ====================================================================================================
+    # calculo y grafico los diferentes scores de verificacion:
+                         
+    bins = np.arange(0,1.1,0.1)
+    route = '/datos/osman/nmme_figuras/'
+    ruta = '/datos/osman/nmme_output/verif_scores/'
+   
+    #RPSS                
+    rpss = verif_scores.RPSS(prob_terc_comb,obs_terciles)
+    titulo = 'Ranked Probabilistic Skill Score'
+    salida = route + 'rpss_'+ file_end + '.eps'
+    verif_scores.plot_scores(lat, lon, rpss, titulo, salida)
+    archivo = 'rpss_' + file_end + '.npz'
+    np.savez(ruta + archivo, rpss = rpss, lat = lat, lon= lon)
+                         
+    #BSS y su descomposicion
+    BSS_above = np.empty([6,nlats,nlons])
+    for i in np.arange(nlats):
+        for j in np.arange(nlons):
+            BSS_above[:,i,j] = verif_scores.BS_decomposition(1-prob_terc_comb[1,:,i,j],obs_terciles[2,:,i,j],bins)
+    archivo = 'bss_above_' + file_end + '.npz'
+    np.savez(ruta + archivo, BSS_above = BSS_above, lat = lat, lon= lon)
+    
+    titulo = 'Brier Skill Score - Above Normal event'             
+    salida = route + 'brierss_above_'+ file_end + '.eps'
+    BSS = 1-BSS_above[0]/(0.33*(1-0.33))
+    verif_scores.plot_scores (lat, lon, BSS , titulo, salida)                     
+    
+    titulo = 'BSS - Resolution - Above Normal event'
+    salida = route + 'bss_res_above_'+ file_end + '.eps'  
+    verif_scores.plot_scores (lat, lon, BSS_above[2,:,:], titulo, salida)
+    
+    titulo = 'BSS - Reliability - Above Normal event'
+    salida = route + 'bss_rel_above_'+ file_end + '.eps'
+    verif_scores.plot_scores (lat, lon, BSS_above[3,:,:], titulo, salida)
+    
+    BSS_below = np.empty([6,nlats,nlons])
+    for i in np.arange(nlats):
+        for j in np.arange(nlons):
+
+            BSS_below[:,i,j] = verif_scores.BS_decomposition(prob_terc_comb[0,:,i,j],obs_terciles[0,:,i,j],bins)
+    archivo = 'bss_below_' + file_end + '.npz'
+    np.savez(ruta+ a rchivo, BSS_below = BSS_below, lat = lat, lon= lon)
+    
+    titulo = 'Brier Skill Score - Below Normal event'
+    salida = route + 'brierss_below_'+ file_end + '.eps'
+    BSS = 1-BSS_below[0]/(0.33*(1-0.33))                        
+    verif_scores.plot_scores (lat, lon, BSS_below[0], titulo, salida)
+    
+    titulo = 'BSS - Resolution - Below Normal event'
+    salida = route + 'bss_res_below_'+ file_end + '.eps'
+    verif_scores.plot_scores (lat, lon, BSS_below[2], titulo, salida)
+    
+    titulo = 'BSS - Reliability - Below Normal event'
+    salida = route + 'bss_rel_below_' + file_end + '.eps'
+    verif_scores.plot_scores (lat, lon, BSS_below[3], titulo, salida)
+                        
+    #AUROC                
+                        
+    auroc_above = verif_scores.auroc(1-prob_terc_comb[1,:,:,:],obs_terciles[2,:,:,:],lat,bins)
+    archivo = 'auroc_above_' + file_end + '.npz'
+    np.savez(ruta + archivo, auroc_above = auroc_above, lat = lat, lon= lon)
+    titulo = 'Area under curve ROC - Above Normal event'
+    salida = route + 'auroc_above_' + file_end + '.eps'
+    verif_scores.plot_scores(lat, lon, auroc_above[:,:], titulo, salida)
+
+    auroc_below = verif_scores.auroc(prob_terc_comb[0,:,:,:], obs_terciles[0,:,:,:], lat, bins)
+    archivo = 'auroc_below_' + file_end + '.npz'
+    np.savez(ruta + archivo, auroc_below = auroc_below, lat = lat, lon= lon)
+    titulo = 'Area under curve ROC - Below Normal event'
+    salida = route + 'auroc_below_'+ file_end + '.eps'
+    verif_scores.plot_scores (lat, lon, auroc_below[:,:], titulo, salida)
+
+    #Reliability y ROC para:
+
+    #todo el dominio
+
+#    salida = varn+'_mme_'+'{:02}'.format(ic_mes)+'_'+'{:02}'.format(plazo
+#            )+'_01_'+'{:03}'.format(p)+'_'+wtech+'_'+ctech+'_hind_all.eps'
+#
+#    verif_scores.rel_roc(prob_terc_comb,obs_terciles,lat,bins,route,salida)
+
+
+    #SA tropical
+
+    #SA extratropical
+#===================================================================================================
+start = time.time()
+
+#abro archivo donde guardo coordenadas
+coordenadas = 'coords'
+lines = [line.rstrip('\n') for line in open(coordenadas)]
+
+coords = {'lat_s' : float(lines[1]),
+        'lat_n' : float(lines [2]),
+        'lon_w' : float(lines[3]),
+        'lon_e' : float(lines[4])}
+
+main()
+end = time.time()
+print(end - start)
+
+# =================================================================================
