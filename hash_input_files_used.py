5acf96343091328d4041bbca3d8e40dbbd2edfb5
Daniel Bonhaure <danielbonhaure@gmail.com>
2022-03-12 19:11:51 -0300

    improved input files control

--- hash_input_files_used.py
+++ hash_input_files_used.py
@@ -5,9 +5,9 @@ import hashlib
 import pathlib
 
 
-def calculate_hash_of_file(file_path):
+def calculate_hash_of_file(a_file_path):
     # Open,close, read file and calculate MD5 on its contents 
-    with open(file_path, 'rb') as file_to_check:
+    with open(a_file_path, 'rb') as file_to_check:
         # read contents of the file
         data = file_to_check.read()    
         # pipe contents of the file through
@@ -18,7 +18,6 @@ def calculate_hash_of_file(file_path):
 
 # ==================================================================================================
 if __name__ == "__main__":
-  
      
     input_file = os.path.join(sys.path[0], "input_files_used")
     output_file = os.path.join(sys.path[0], f"{input_file}_with_hashes")
